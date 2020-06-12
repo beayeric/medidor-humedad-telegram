5d04036a824c7ddf181545e83707cdf021c65be4
beayeric <bye@iMac-de-ByE.local>
2020-06-12 16:17:38 +0200

    actualizado

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,40 @@
+/**
+ * Primeras pruebas de intento de lectura de la temperatura y humedad desde WeMos D1 Mini 
+ * desde un bot de telegram.
+ * */
+
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <UniversalTelegramBot.h>
 #include <WiFiClientSecure.h>
-#include "DHT.h"
-#include <Adafruit_Sensor.h>
+#include <UniversalTelegramBot.h> //Incluimos la librería para  Telegram  - https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
+#include <ArduinoJson.h> // Incluimos la librería Json para el uso de la API de Telegram ATENCIÓN : Utilizar la versión 5.x.x porque las siguientes no funcioncionan -  https://github.com/bblanchon/ArduinoJson
+#include "DHT.h" // Librería para el sensor  V1.3.4
+#include <Adafruit_Sensor.h> //Librería para la lectura del sensor V1.0.3
 
-//Introducimos los datos de la red a la que se conectara
-//We enter the data of the network to which you will connect
 
-const char* ssid = "xxxx";
-const char* password = "xxxx";
 
-//Configuramos la conexión con telegram
-//We set up the telegram connection
-const char BotToken[] = "xxxxxxxxxxxxxxxxx";
+//------- DATOS PARA LA CONEXIÓN AL WIFI Y BOT DE TELEGRAM ------//
+
+char ssid[] = "nombre de la red";         		  // el nombre de su red SSID
+char password[] = "contraseña de la red";       // la contraseña de su red
+
+#define TELEGRAM_BOT_TOKEN "token del bot de telegram"  // TOKEN proporcionado por BOTFATHER
+
+//------- ---------------------- ------//
 
 WiFiClientSecure client;
-UniversalTelegramBot bot (BotToken, client);
+UniversalTelegramBot bot(TELEGRAM_BOT_TOKEN, client);
 
-int Bot_mtbs = 1000; //mean time between scan messages
-long Bot_lasttime;   //last time messages' scan has been done
+int Bot_mtbs = 1000; // tiempo medio entre escaneo de mensajes
+long Bot_lasttime;   // la última vez que se realizó la exploración de mensajes
 bool Start = false;
 
+// ----------- Funciones de temperatura -----------//
 
-//----------------------------------------------------------------------------------
-//Funciones de toma de temperatura
-
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321 // definimos el sensor de temperatura y humedad
-const int DHTPin = D4;     // el pin donde esta conectado el sensor
- 
+#define DHTTYPE DHT22   // TIPO DE SENSOR
+const int DHTPin = D4;  // Pin donde esta conectado ( por defecto en nuestro proyecto esta conectado en el pin 4)
 DHT dht(DHTPin, DHTTYPE);
 
-//----------------------------------------------------------------------------------
-//Configuramos el bot de telegram --- We set up the telegram bot 
 
 void handleNewMessages(int numNewMessages) {
   Serial.println("handleNewMessages");
@@ -45,32 +47,75 @@ void handleNewMessages(int numNewMessages) {
     String from_name = bot.messages[i].from_name;
     if (from_name == "") from_name = "Guest";
 
-    if (text == "/estado") {
-      teclapower();  /// esta es la función de Pablo .
-      
-      bot.sendMessage(chat_id, "El estado es:");  
 
-   
-    if (text == "/options") {
-      String keyboardJson = "[[\"/estado\"]";
-      bot.sendMessageWithReplyKeyboard(chat_id, "Elige una de estas opciones", "", keyboardJson, true);
+//--------------Configurar estados y respuestas del bot de telegram-----------------------//
+
+
+    if (text == "/estado") {
+      float h = dht.readHumidity();
+      float t = dht.readTemperature();
+      bot.sendMessage(chat_id, "El estado de le temperatura y la humedad es:", "");
+    }
+
+    if (text == "/temperatura") {
+      float t = dht.readTemperature();
+      bot.sendMessage(chat_id, "La temperatura en sala es de:", "");
+    }
+
+    if (text == "/humedad") {
+      dht.readHumidity();
+      bot.sendMessage(chat_id, "La temperatura en sala es de:", "");
+    }
+
+    }
+
+    if (text == "/inicio") {
+      String welcome = "Hola" + from_name + "esta es tu sale de control" ".\n";
+      welcome += "Esta es una prueba de medidor de humedad y temperatura con bot de telegram.\n\n";
+      welcome += "/humedad : para saber el nivel de humedad relativa\n";
+      welcome += "/temperatura : para saber el estado de la temperatura\n";
+      welcome += "/estado : para saber estado de la humedad y la temperatura al mismo tiempo\n";
+      bot.sendMessage(chat_id, welcome, "Markdown");
     }
   }
-}  
- 
-void setup() {
-   Serial.begin(9600);
-   Serial.println("Test de sonda!");
- 
-   dht.begin();
-
-   // TODO: Aqui igual debería conectarse al WIFI ¿no?
 }
- 
+
+
+void setup() {
+  Serial.begin(115200);
+
+  // Establezca WiFi en modo estación y desconéctese de un AP si era anteriormente estaba conectado
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  // Intente conectarse a la red Wifi
+  Serial.print("Connecting Wifi: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  delay(10);
+  digitalWrite(LED_BUILTIN, HIGH); // el led OFF al inicio
+
+  client.setInsecure();
+
+}
+
 void loop() {
-   
-   if (millis() > Bot_lasttime + Bot_mtbs)  {
+  if (millis() > Bot_lasttime + Bot_mtbs)  {
     int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
     while(numNewMessages) {
       Serial.println("got response");
       handleNewMessages(numNewMessages);
@@ -79,11 +124,11 @@ void loop() {
 
     Bot_lasttime = millis();
   }
-   
-   // Wait a few seconds between measurements.
-   delay(4000);
+
+// ----- funciones para medir con el sensor ---//
+  delay(4000); // tiempo de espera entre lecturas
  
-   // Reading temperature or humidity takes about 250 milliseconds!
+  
    float h = dht.readHumidity();
    float t = dht.readTemperature();
  
@@ -99,4 +144,6 @@ void loop() {
    Serial.print("La temperatura es de: ");
    Serial.print(t);
    Serial.print(" *C ");
+}
+
 }
