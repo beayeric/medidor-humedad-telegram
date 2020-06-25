4c5ed65202ac80a6f14a0d4907a8ede0407e2d30
moisesfa <moifdez@gmail.com>
2020-06-25 20:33:35 +0200

    opcion version 0.0.1

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,6 @@
   Pinde de conexión del dht22:
   D4
  
-
 */
 
 #include <Arduino.h>
@@ -28,16 +27,15 @@ char password[] = "xxxx";       // la contraseña de su red
 
 #define TELEGRAM_BOT_TOKEN "xxxxx"  // TOKEN proporcionado por BOTFATHER
 
-
 //------- ---------------------- ------//
 
 WiFiClientSecure client;
 UniversalTelegramBot bot(TELEGRAM_BOT_TOKEN, client);
 
-int Bot_mtbs = 1000;  // tiempo medio entre escaneo de mensajes
-long Bot_lasttime;   // la última vez que se realizó la exploración de mensajes
-int dht_mtbs = 1000; // tiempo entre lecturas.
-long hBdt_lasttime;  // la última vez que se realizó la exploración de mensajes
+int Bot_mtbs = 1000;    // tiempo medio entre escaneo de mensajes
+long Bot_lasttime;      // la última vez que se realizó la exploración de mensajes
+int dht_mtbs = 5000;    // tiempo entre lecturas 5 segundos
+long hBdt_lasttime;     // la última vez que se realizó la exploración de mensajes
 bool Start = false;
 
 
@@ -67,23 +65,23 @@ void handleNewMessages(int numNewMessages) {
     if (from_name == "") from_name = "Guest";
 
 // --- comienza la parte que se puede personalizar -- //
+// Respuesta a los mensajes del bot
 
     if (text == "/estado") { 
       String resultado = str_hum +"\n";
       resultado += str_tem +"\n";
       bot.sendMessage(chat_id, resultado, "");
-      
-      
     }
+
     if (text == "/temperatura") {
       bot.sendMessage(chat_id, str_tem , "");
     }
+
     if (text == "/humedad") {
       bot.sendMessage(chat_id, str_hum , "");
     }
 
-    
-    if (text == "/inicio") {
+    if (text == "/start") {
       String welcome = "Hola " + from_name + " esta es tu sala de control" ".\n";
       welcome += "Esta es una prueba de medidor de humedad y temperatura con bot de telegram.\n\n";
       welcome += "/humedad : para saber el nivel de humedad relativa\n";
@@ -103,7 +101,7 @@ void setup() {
   WiFi.disconnect();
   delay(100);
 
-  // Intente conectarse a la red Wifi
+  // Intenta conectarse a la red Wifi
   Serial.print("Connecting Wifi: ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
@@ -112,19 +110,27 @@ void setup() {
     Serial.print(".");
     delay(500);
   }
-// -- valores que se muestran en el monitor serie --//
+  
+  // -- valores que se muestran en el monitor serie --//
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
+  // Comunicación de manera no segura
+  //client.setInsecure();
+  //bot._debug = true; // para ver el funcionamiento del bot
 
-  client.setInsecure();
+  // Comunicación de manera segura
+  bot._debug = true; // para ver el funcionamiento del bot
+  const uint8_t fingerprint[20] = { 0xF2, 0xAD, 0x29, 0x9C, 0x34, 0x48, 0xDD, 0x8D, 0xF4, 0xCF, 0x52, 0x32, 0xF6, 0x57, 0x33, 0x68, 0x2E, 0x81, 0xC1, 0x90 };
+  client.setFingerprint(fingerprint);
 
 }
 
 void loop() {
 
+  // Cada tiempo definido en Bot_mtbs vemos si se recibe algún mensaje
   if (millis() > Bot_lasttime + Bot_mtbs)  {
     int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
 
@@ -137,8 +143,8 @@ void loop() {
     Bot_lasttime = millis();
   }
 
-  
-    if (millis() > hBdt_lasttime + dht_mtbs)  {
+  // Cada tiempo definido en dht_mtbs leemos el sensor de temperatura/humedad
+  if (millis() > hBdt_lasttime + dht_mtbs)  {
     
     float h = dht.readHumidity();
     float t = dht.readTemperature();
@@ -148,19 +154,15 @@ void loop() {
         return;
     }
 
-   
     str_tem = "Temperatura : " + String(t, 2);   
     Serial.println(str_tem);
     str_hum = "Humedad : " + String(h, 2);
     Serial.println(str_hum);
-
-  
-   Serial.print(h);
-   Serial.print(t);
-  
-  hBdt_lasttime = millis();
-
+    
+    hBdt_lasttime = millis();
   }
+
 }
+
 
     
